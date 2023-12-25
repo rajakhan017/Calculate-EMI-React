0bfa15e37efb7d2fd1a8e362156f674f632071e6
Khan <imamul.khan@openreach.co.uk>
2023-12-25 23:02:23 +0530

    Calculate EMI

---
+++ src/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+	return (
+		<header className='h-[10vh] lg:px-12 bg-crimson-600 flex justify-center lg:justify-start items-center'>
+			<h2 className='text-2xl font-bold text-white'>Bank of React</h2>
+		</header>
+	)
+}
+export default Header
